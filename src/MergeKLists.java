553260bcdf5ae19aa420fa239dd92ac5629db45f
VIVILL <15170211043@163.com>
2023-02-11 17:33:32 +0800

    添加23.合并K个升序链表
    (cherry picked from commit d87ae8c49002d55deb4b467627772750661f1093)

---
+++ src/MergeKLists.java
@@ -0,0 +1,37 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+//https://leetcode.cn/problems/merge-k-sorted-lists/comments/64343
+public class MergeKLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        ListNode dummyHead = new ListNode(0);
+        ListNode curr = dummyHead;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+
+        for (ListNode list : lists) {
+            if (list == null) {
+                continue;
+            }
+            pq.add(list);
+        }
+
+        while (!pq.isEmpty()) {
+            ListNode nextNode = pq.poll();
+            curr.next = nextNode;
+            curr = curr.next;
+            if (nextNode.next != null) {
+                pq.add(nextNode.next);
+            }
+        }
+        return dummyHead.next;
+    }
+}
+
--- src/Test.java
+++ src/Test.java
@@ -11,6 +11,15 @@ public class Test {
 //        int[] nums = {-2,0,0,2,2};
         int[] nums = {-1,0,1,2,-1,-4};
         System.out.println("result  = " + sum.threeSum(nums));
+
+// 输入 [[1,listNode2],[1,listNode3],[2,ListNode(6)]]  输出 [1,1,2,6]
+        MergeKLists mergeKLists = new MergeKLists();
+        ListNode listNode3 = new ListNode(2,new ListNode(6));
+        ListNode listNode2 = new ListNode(1,listNode3);
+        ListNode listNode1 = new ListNode(1,listNode2);
+        ListNode[] listNodes = {listNode1, listNode2,listNode3};
+        // 打断点可查看生成的数据 dummyHead.next
+        mergeKLists.mergeKLists(listNodes);
     }
 
 
