a090366c2522c203f0f5596a7a1fb1894924d1a2
VIVILL <VIVILL@users.noreply.github.com>
2023-02-12 10:01:29 +0800

    添加20. 有效的括号
    (cherry picked from commit d8dea13824ae342b8a75fc8ff916360527c86866)

---
+++ src/ValidParentheses.java
@@ -0,0 +1,44 @@
+
+import java.util.Stack;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 每个右括号都有一个对应的相同类型的左括号。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ValidParentheses {
+    public static void main(String[] args) {
+//        String s = "(){";
+        String s = "{([])}";
+        System.out.println("isValid  = " + isValid(s));
+
+    }
+    //https://leetcode.cn/problems/valid-parentheses/solution/valid-parentheses-fu-zhu-zhan-fa-by-jin407891080/170276
+    public static boolean isValid(String s) {
+        if(s.isEmpty())
+            return true;
+        Stack<Character> stack=new Stack<Character>();
+        for(char c:s.toCharArray()){
+            if(c=='(')
+                stack.push(')');
+            else if(c=='{')
+                stack.push('}');
+            else if(c=='[')
+                stack.push(']');
+            else if(stack.empty()||c!=stack.pop())
+                return false;
+        }
+        if(stack.empty())
+            return true;
+        return false;
+    }
+
+}
