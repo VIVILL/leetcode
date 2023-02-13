d8cc3cc553faf0924c07cb5177258572b18aee47
VIVILL <VIVILL@users.noreply.github.com>
2023-02-13 14:35:40 +0800

    添加200. 岛屿数量

---
+++ src/Graph/NumberOfIslands.java
@@ -0,0 +1,89 @@
+package Graph;
+
+/**
+ * https://leetcode.cn/problems/number-of-islands/description/
+ * 200. 岛屿数量
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ *
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ *
+ * 此外，你可以假设该网格的四条边均被水包围。
+ * 示例 1：
+ *
+ * 输入：grid = [
+ *   ["1","1","1","1","0"],
+ *   ["1","1","0","1","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","0","0","0"]
+ * ]
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：grid = [
+ *   ["1","1","0","0","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","1","0","0"],
+ *   ["0","0","0","1","1"]
+ * ]
+ * 输出：3
+ */
+public class NumberOfIslands {
+
+    public static void main(String[] args) {
+        //[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+        char[] char1 = {'1','1','1','1','0'};
+        char[] char2 = {'1','1','0','1','0'};
+        char[] char3 = {'1','1','0','0','0'};
+        char[] char4 = {'0','0','0','0','0'};
+        char[][] chars0 = {char1,char2,char3,char4};
+        System.out.println("numIslands = "+ numIslands(chars0));
+       // numIslands = 1
+
+        char[] char5 = {'1','1','0','0','0'};
+        char[] char6 = {'1','1','0','0','0'};
+        char[] char7 = {'0','0','1','0','0'};
+        char[] char8 = {'0','0','0','1','1'};
+        char[][] chars = {char5,char6,char7,char8};
+        System.out.println("numIslands = "+ numIslands(chars));
+        // numIslands = 3
+    }
+
+    // https://leetcode.cn/problems/number-of-islands/solution/number-of-islands-shen-du-you-xian-bian-li-dfs-or-/
+    // 深度优先遍历 DFS
+    /*
+思路一：深度优先遍历 DFS
+目标是找到矩阵中 “岛屿的数量” ，上下左右相连的 1 都被认为是连续岛屿。
+dfs方法： 设目前指针指向一个岛屿中的某一点 (i, j)，寻找包括此点的岛屿边界。
+从 (i, j) 向此点的上下左右 (i+1,j),(i-1,j),(i,j+1),(i,j-1) 做深度搜索。
+终止条件：
+(i, j) 越过矩阵边界;
+grid[i][j] == 0，代表此分支已越过岛屿边界。
+搜索岛屿的同时，执行 grid[i][j] = '0'，即将岛屿所有节点删除，以免之后重复搜索相同岛屿。
+主循环：
+遍历整个矩阵，当遇到 grid[i][j] == '1' 时，从此点开始做深度优先搜索 dfs，岛屿数 count + 1 且在深度优先搜索中删除此岛屿。
+最终返回岛屿数 count 即可。
+
+    * */
+    public static int numIslands(char[][] grid) {
+        int count = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static void dfs(char[][] grid, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+    }
+
+}
